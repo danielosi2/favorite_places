1a69d3a7bff399dd23795bf63e957944c5d9aa7c
Osigbemhe Daniel <90054585+danielosi2@users.noreply.github.com>
2023-10-09 20:52:40 +0100

    sql

--- lib/provider/places_provider.dart
+++ lib/provider/places_provider.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
-
+import 'package:path/path.dart' as path;
 import 'package:favorite_places/models/places.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class PlaceNotifier extends StateNotifier<List<Places>> {
   PlaceNotifier() : super([]);
@@ -10,12 +13,19 @@ class PlaceNotifier extends StateNotifier<List<Places>> {
     String title,
     File image,
     PlaceLocation location,
-  ) {
+  ) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
     final addPlace = Places(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    sql.openDatabase(path.join(dbPath, ''));
     state = [...state, addPlace];
   }
 }
